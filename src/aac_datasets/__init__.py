33d4c2327684b1600ac7c24f03eaa70a94baf4bd
Labbeti <etienne.labbe31@gmail.com>
2022-05-19 11:22:46 +0200

    Add: Imports in __init__.

--- src/aac_datasets/__init__.py
+++ src/aac_datasets/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .audiocaps import AudioCaps
+from .clotho import Clotho
+from .macs import MACS
+
+
 __author__ = "Etienne Labbé (Labbeti)"
 __name__ = "aac_datasets"
 __version__ = "0.1.0"
