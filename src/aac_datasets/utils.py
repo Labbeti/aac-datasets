3aa9296d4457d6ff88f2be7fb9b61818c2c62767
Labbeti <etienne.labbe31@gmail.com>
2022-05-24 17:05:37 +0200

    Del: Remove download function in utils.

--- src/aac_datasets/utils.py
+++ src/aac_datasets/utils.py
@@ -8,33 +8,6 @@ from typing import Any
 from torch import nn, Tensor
 from torch.nn.utils.rnn import pad_sequence
 
-from aac_datasets.audiocaps import AudioCaps
-from aac_datasets.clotho import Clotho
-from aac_datasets.macs import MACS
-
-
-def download_audiocaps(
-    root: str = ".",
-    verbose: int = 1,
-    ffmpeg: str = "ffmpeg",
-    youtube_dl: str = "youtube-dl",
-    load_tags: bool = True,
-) -> None:
-    AudioCaps.FFMPEG_PATH = ffmpeg
-    AudioCaps.YOUTUBE_DL_PATH = youtube_dl
-
-    for subset in AudioCaps.SUBSETS:
-        _ = AudioCaps(root, subset, download=True, verbose=verbose, load_tags=load_tags)
-
-
-def download_clotho(root: str = ".", verbose: int = 1, version: str = "v2.1") -> None:
-    for subset in Clotho.SUBSETS_DICT[version]:
-        _ = Clotho(root, subset, download=True, verbose=verbose, version=version)
-
-
-def download_macs(root: str = ".", verbose: int = 1) -> None:
-    _ = MACS(root, download=True, verbose=verbose)
-
 
 class Collate:
     def __init__(self, pad_value: float = 0.0) -> None:
