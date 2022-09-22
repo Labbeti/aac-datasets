f959a6930373fd169ab840c432f5dd5b79ba2e92
Labbeti <etienne.labbe31@gmail.com>
2022-09-22 10:33:59 +0200

    Mod: Update entrypoints names.

--- src/aac_datasets/__main__.py
+++ src/aac_datasets/__main__.py
@@ -5,10 +5,10 @@
 def _print_usage() -> None:
     print(
         "Command line usage :\n"
-        "- Download a dataset             : aac-ds-download [--root ROOT] [--verbose VERBOSE] [--force (false|true)] (clotho|audiocaps|macs) [ARGS...]\n"
-        "- Check a installation directory : aac-ds-check [--root ROOT] [--verbose VERBOSE]\n"
-        "- Print package install info     : aac-ds-info\n"
-        "- Show this usage page           : aac-ds\n"
+        "- Download a dataset             : aac-datasets-download [--root ROOT] [--verbose VERBOSE] [--force (false|true)] (clotho|audiocaps|macs) [ARGS...]\n"
+        "- Check a installation directory : aac-datasets-check [--root ROOT] [--verbose VERBOSE]\n"
+        "- Print package install info     : aac-datasets-info\n"
+        "- Show this usage page           : aac-datasets\n"
     )
 
 
