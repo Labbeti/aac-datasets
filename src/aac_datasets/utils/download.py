00cd0de868bfb19b00c08f81bb4059e0ae233af3
Labbeti <etienne.labbe31@gmail.com>
2023-04-20 15:49:05 +0200

    Fix: Default chunk size for verify files to 256MiB.

--- src/aac_datasets/utils/download.py
+++ src/aac_datasets/utils/download.py
@@ -8,6 +8,7 @@ from typing import Union
 
 
 HASH_TYPES = ("sha256", "md5")
+DEFAULT_CHUNK_SIZE = 256 * 1024**2
 
 
 def validate_file(
@@ -22,13 +23,20 @@ def validate_file(
     :param hash_type: The hash type. defaults to "sha256".
     :returns: True if the file hash corresponds to the hash value.
     """
-    file_hash_value = _hash_file(fpath, hash_type)
-    is_valid = file_hash_value == hash_value
+    hash_value_found = _hash_file(fpath, hash_type)
+    is_valid = hash_value_found == hash_value
     return is_valid
 
 
-def _hash_file(fpath: Union[str, Path], hash_type: str, chunk_size: int = 1028) -> str:
-    """Return the hash value for a file."""
+def _hash_file(
+    fpath: Union[str, Path],
+    hash_type: str,
+    chunk_size: int = DEFAULT_CHUNK_SIZE,
+) -> str:
+    """Return the hash value for a file.
+
+    FROM https://github.com/pytorch/audio/blob/v0.13.0/torchaudio/datasets/utils.py#L110
+    """
     if hash_type == "sha256":
         hasher = hashlib.sha256()
     elif hash_type == "md5":
@@ -39,11 +47,11 @@ def _hash_file(fpath: Union[str, Path], hash_type: str, chunk_size: int = 1028) 
         )
 
     with open(fpath, "rb") as file:
-        is_valid_chunk = True
-        while is_valid_chunk:
+        while True:
             chunk = file.read(chunk_size)
+            if not chunk:
+                break
             hasher.update(chunk)
-            is_valid_chunk = bool(chunk)
 
     hash_value = hasher.hexdigest()
     return hash_value
