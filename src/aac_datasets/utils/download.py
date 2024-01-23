36a60aca6b9a8fdb50929a5e5025e97ad7cc2ef5
Labbeti <etienne.labbe31@gmail.com>
2024-01-23 14:56:14 +0100

    Add: Verbose option in safe_rmdir func.

--- src/aac_datasets/utils/download.py
+++ src/aac_datasets/utils/download.py
@@ -8,6 +8,8 @@ import os.path as osp
 from pathlib import Path
 from typing import List, Union
 
+import tqdm
+
 from torch.hub import download_url_to_file
 
 
@@ -33,6 +35,7 @@ def safe_rmdir(
     rm_root: bool = True,
     error_on_non_empty_dir: bool = True,
     followlinks: bool = False,
+    verbose: int = 0,
 ) -> List[str]:
     """Remove all empty sub-directories.
 
@@ -40,18 +43,21 @@ def safe_rmdir(
     :param rm_root: If True, remove the root directory too. defaults to True.
     :param error_on_non_empty_dir: If True, raises a RuntimeError if a subdirectory contains at least 1 file. Otherwise it will leave non-empty directories. defaults to True.
     :param followlinks: Indicates whether or not symbolic links shound be followed. defaults to False.
+    :param verbose: Verbose level. defaults to 0.
     :returns: The list of directories paths deleted.
     """
     to_delete = []
 
-    for dpath, dnames, fnames in os.walk(root, topdown=False, followlinks=followlinks):
+    for dpath, dnames, fnames in tqdm.tqdm(
+        os.walk(root, topdown=False, followlinks=followlinks),
+        disable=verbose < 2,
+    ):
         if not rm_root and dpath == root:
             continue
         elif len(fnames) == 0 and len(dnames) == 0:
             to_delete.append(dpath)
         elif error_on_non_empty_dir:
-            print(f"{to_delete=}")
-            raise RuntimeError(f"Cannot remove non-empty directory {dpath}.")
+            raise RuntimeError(f"Cannot remove non-empty directory '{dpath}'.")
 
     for dpath in to_delete:
         os.rmdir(dpath)
