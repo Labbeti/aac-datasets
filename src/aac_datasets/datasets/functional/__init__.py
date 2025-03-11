86fa06f5899a0cbe248f61990616ab6f44d64d26
Labbeti <labbeti.pub@gmail.com>
2025-03-11 14:44:07 +0100

    Add: docstring in init.

--- src/aac_datasets/datasets/functional/__init__.py
+++ src/aac_datasets/datasets/functional/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Functional interface to load/download datasets.
+
+Each module contains the its own logic for data preparation, but returns a similar dict structure for each dataset class.
+"""
+
 from .audiocaps import AudioCapsCard, load_audiocaps_dataset, download_audiocaps_dataset
 from .clotho import ClothoCard, load_clotho_dataset, download_clotho_dataset
 from .macs import MACSCard, load_macs_dataset, download_macs_dataset
